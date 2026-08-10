3dc44b4f0ceb44652d0b95e8b4ee1697a0fb4853
Bhuvan92-cyber <chbhuvansri02@gmail.com>
2026-08-10 21:10:59 +0530

    fix10

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const config = {
   port: Number(process.env.PORT ?? 3001),
-  clientOrigins: (process.env.CLIENT_ORIGINS ?? 'http://localhost:8081')
+  clientOrigins: (process.env.CLIENT_ORIGINS ?? 'https://realtimechatapp-e08b.onrender.com')
     .split(',')
     .map((origin) => origin.trim())
     .filter(Boolean),
